649cf51673e073104499e84b446b65ff2df69004
shabrinabdat <shabyyy12@gmail.com>
2023-02-23 16:52:03 +0700

    update login.page

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,6 +1,6 @@
 import Page from "./page";
 
-class LoginPage extends page {
+class LoginPage extends Page {
     //selector
     get signInSelector() {
         return $('/html/body/header/div[2]/div[1]/div[1]/div[3]/div[1]/a[1]')
